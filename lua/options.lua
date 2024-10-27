262a77556fe8369de1a1099a67e902ffe9f9d914
andre <tanandre93@gmail.com>
2024-10-27 09:53:31 +0800

    Enable incsearch, configure folding

--- lua/options.lua
+++ lua/options.lua
@@ -58,3 +58,10 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 vim.opt.autoread = true
+
+-- UFO folding
+vim.opt.foldcolumn = '1' -- '0' is not bad
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
