8d1f5325223ec6147f05a674c22c673abaece15f
andre <tanandre93@gmail.com>
2024-10-27 09:50:40 +0800

    Add linters

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        python = { "flake8" },
+        python = { 'flake8' },
+        dockerfile = { 'hadolint' },
+        json = { 'jsonlint' },
+        text = { 'vale' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
